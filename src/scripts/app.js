f6abaafbc84484ad276c23fef2d9177d86ed7896
Nalu-devs <pordosol699@gmail.com>
2026-03-23 11:33:18 -0300

    test code review

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,6 @@
 import { signup, loginUsuario } from "./auth.js";
+import { carregarMaterias, carregarConteudos } from "./genereAsk.js";
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const signupForm = document.getElementById("signupForm");
@@ -36,4 +38,80 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     }
+});
+
+
+
+document.getElementById("formPergunta").addEventListener("submit", async function (notReaload) {
+    notReaload.preventDefault();
+    const idMateria = document.getElementById("materia").value;
+    const idConteudo = document.getElementById("conteudo").value;
+    const pergunta = document.getElementById("pergunta").value;
+
+    if (!idConteudo || !pergunta || !idMateria) {
+        alert("SELECIONA A PORRA DO CONTEUDO OU DA PERGUNTA KRL");
+        return;
+    }
+
+    console.log("idMateria:", idMateria, "idConteudo:", idConteudo);
+
+    const alternativas = [
+        document.getElementById("alt1").value,
+        document.getElementById("alt2").value,
+        document.getElementById("alt3").value,
+        document.getElementById("alt4").value,
+        document.getElementById("alt5").value,
+    ];
+
+    const correta = document.getElementById("correta").value;
+
+    if (alternativas.some(a => a.trim() === "")) {
+            alert("Preencha todas as alternativas!");
+            return;
+        }
+
+    try {
+        const { data: perguntaCriada } = await supabaseClient
+            .from("perguntas")
+            .insert([
+                {
+                    pergunta_texto: pergunta,
+                    id_conteudo: idConteudo,
+                    id_materia: idMateria
+                }
+            ])
+            .select();
+
+        if (!perguntaCriada || perguntaCriada.length === 0) {
+            alert("Erro ao criar pergunta");
+            return;
+        }
+
+        const idPergunta = perguntaCriada[0].id_pergunta;
+
+        for (let i = 0; i < alternativas.length; i++) {
+            await supabaseClient
+                .from("alternativa")
+                .insert([
+                    {
+                        nome_alternativa: alternativas[i],
+                        id_pergunta: idPergunta,
+                        correta: (i + 1) == correta
+                    }
+                ]);
+        }
+
+        alert("Pergunta cadastrada");
+    } catch (erro) {
+        console.error(erro);
+        alert("Erro ao cadastrar pergunta");
+    }
+    document.getElementById("pergunta").value = "";
+    document.getElementById("alt1").value = "";
+    document.getElementById("alt2").value = "";
+    document.getElementById("alt3").value = "";
+    document.getElementById("alt4").value = "";
+    document.getElementById("alt5").value = "";
+    document.getElementById("conteudo").value = "";
+    document.getElementById("materia").value = "";
 });
---
+++ src/scripts/genereAsk.js
@@ -0,0 +1,42 @@
+export async function carregarMaterias() {
+    const { data, error } = await supabaseClient
+        .from("materia")
+        .select("id_materia, nome_materia");
+    if (error) {
+        console.error("Erro:", error);
+        return;
+    }
+    const select = document.getElementById("materia");
+    data.forEach(materia => {
+        const option = document.createElement("option");
+        option.value = materia.id_materia;
+        option.textContent = materia.nome_materia;
+        select.appendChild(option);
+    });
+}
+carregarMaterias();
+
+
+document.getElementById("materia").addEventListener("change", carregarConteudos);
+
+export async function carregarConteudos() {
+    const idMateria = document.getElementById("materia").value;
+    if (!idMateria) return;
+
+    const { data, error } = await supabaseClient
+        .from("conteudo")
+        .select("id_conteudo, nome_conteudo")
+        .eq("id_materia", idMateria)
+    if (error) {
+        console.error("Erro:", error);
+        return;
+    }
+    const select = document.getElementById("conteudo");
+    select.innerHTML = '<option value="">Selecione o conteúdo</option>';
+    data.forEach(conteudo => {
+        const option = document.createElement("option");
+        option.value = conteudo.id_conteudo;
+        option.textContent = conteudo.nome_conteudo;
+        select.appendChild(option);
+    });
+}
