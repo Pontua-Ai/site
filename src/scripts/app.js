c45460364f26a9db8918fdf1b6a381a78b793044
Nalu-devs <pordosol699@gmail.com>
2026-03-22 21:12:17 -0300

    validção de login

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,18 +1,40 @@
-import { signup } from "./auth.js";
+import { signup, loginUsuario } from "./auth.js";
 
-document.getElementById("signupForm").addEventListener("submit", async (event) => { 
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", () => {
+    const signupForm = document.getElementById("signupForm");
+    if (signupForm) {
+        signupForm.addEventListener("submit", async (event) => { 
+            event.preventDefault();
 
-    const name = document.getElementById("username").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("senha").value;
+            const name = document.getElementById("username").value;
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("senha").value;
 
-    const result = await signup(name, email, password);
+            const result = await signup(name, email, password);
 
-    if (result && result.success) {
-        alert("Cadastro realizado com sucesso!");
-        window.location.href = "inicio.html";
-    } else {
-        alert("Erro ao realizar cadastro: " + (result?.error || "Erro desconhecido"));
+            if (result && result.success) {
+                alert("Cadastro realizado com sucesso!");
+                window.location.href = "inicio.html";
+            } else {
+                alert("Erro ao realizar cadastro: " + (result?.error || "Erro desconhecido"));
+            }
+        });
+    }
+
+    const loginForm = document.getElementById("loginForm");
+    if (loginForm) {
+        loginForm.addEventListener("submit", async (event) => {
+            event.preventDefault();
+            const login = document.getElementById("loginEmail").value;
+            const password = document.getElementById("loginPassword").value;
+            const result = await loginUsuario(login, password);
+
+            if (result && result.success) {
+                alert("Login realizado com sucesso!");
+                window.location.href = "sobre.html";
+            } else {
+                alert("Erro ao realizar login: " + (result?.error || "Erro desconhecido"));
+            }
+        });
     }
 });
