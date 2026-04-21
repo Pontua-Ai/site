1157e64d45cca19e955cc0f34e89e54fa0e18539
Nalu-devs <pordosol699@gmail.com>
2026-04-20 23:58:29 -0300

    adiconando theme no sobre

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -21,6 +21,21 @@ function updateThemeLabel() {
         checkbox.checked = isDark;
     }
     
+    const themeIcon = document.querySelector(".theme-icon");
+    if (themeIcon) {
+        if (isDark) {
+            themeIcon.className = "material-icons theme-icon";
+            themeIcon.textContent = "light_mode";
+            themeIcon.style.cursor = "pointer";
+            themeIcon.style.fontSize = "1.2rem";
+        } else {
+            themeIcon.className = "fa-solid fa-moon theme-icon";
+            themeIcon.textContent = "";
+            themeIcon.style.cursor = "pointer";
+            themeIcon.style.fontSize = "1.2rem";
+        }
+    }
+    
     const cards = document.querySelectorAll(".cardConfi");
     for (const card of cards) {
         const text = card.querySelector("p");
