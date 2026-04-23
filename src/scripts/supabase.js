b80e08f8248a725dd3782bae9ccbfa14c828cae2
Nalu-devs <pordosol699@gmail.com>
2026-04-23 09:48:58 -0300

    aplicando nova credencial no supabase e deixando ela no gitgnore

--- src/scripts/supabase.js
+++ src/scripts/supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { config } from "./config.js";
 
-const supabaseURL = "https://nsacgpcnhqzxassfvuts.supabase.co";
-const supabaseKey = "sb_publishable_h7E_Yp2W314kFCmGo4qR5g_C8ctFv8n";
-
-const supabaseClient = createClient(supabaseURL, supabaseKey);
+const supabaseClient = createClient(config.SUPABASE_URL, config.SUPABASE_KEY);
 
 export default supabaseClient;
